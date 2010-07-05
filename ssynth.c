9c49a880e2fa547b516bb21ca0ab8b7abb7f2b31
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-07-05 19:50:19 -0400

    Some code cleanup and added support for velocity.

--- ssynth.c
+++ ssynth.c
@@ -5,16 +5,16 @@
    noteon and noteoff are used.
 
   * TODO Percussion!
-  * TODO Modulation, pitchbend, and vibrato.  Drop this if it's complicated
-         we want to keep the implementation simple.
+  * TODO Modulation, pitchbend, and vibrato.  Drop this if it's
+         complicated; we want to keep the implementation as simple as
+         possible.
   * TODO Look for ways to simplify/shorten the code.
-  * TODO Check for failed ioctl() and fcntl() calls.
+  * TODO Check for failed ioctl() calls.
 */
 
 #include <stdbool.h>
 #include <unistd.h>
 #include <linux/soundcard.h>
-#include <fcntl.h>
 #include <stropts.h>
 #include <math.h>
 #include <sys/select.h>
@@ -23,22 +23,29 @@
 #include <stdlib.h> // exit()
 #define NSYNTHS 8
 
-typedef struct notes {
-	double percent; // progress through waveform
-	double inc; // Frequency: percent of waveform per sample;
-} N;
+typedef struct synth {
+	double percent; // - progress through waveform
+	double inc;     // - percent of waveform per sample;  Should be < 1
+	double vol;	    // - Value of the waveform during the high part.  The
+                   //   biggest value we can send is 255.
+} S;
 
+// for ioctl()
 int samplerate = 44100;
 int samplesize = AFMT_U8;
-double freq = 440; // hertz
 int mono = 1;
-int usednotes = 0;
 int fragment = 0x00040009; // TODO What does this mean? (this was lifted from
                            //      the oss softsynth example)
-N ns[NSYNTHS];
+
+// synth state
+int usednotes = 0;
+struct synth ns[NSYNTHS];
+double freq = 440; // hertz
 
 #define err(...) fprintf(stderr, __VA_ARGS__), exit(1)
 #define perr(x) perror(x), exit(1)
+#define MAX(x, y) (x>y)?x:y
+
 void send (byte s) {
  top:
 	switch (write(1, &s, 1)) {
@@ -48,13 +55,15 @@ void send (byte s) {
 		default: err("wtf"); }}
 
 byte nextsample () {
-	byte sample = 0;
-	int i;
-	for (i=0; i<usednotes; i++) {
-		ns[i].percent += ns[i].inc;
-		while (ns[i].percent > 1) ns[i].percent -= 1;
-		if (ns[i].percent <= 0.5) sample += 255/NSYNTHS; }
-	return sample; }
+	int remain=usednotes;
+	double sample = 0.0;
+	for (struct synth *s=ns; remain; s++, remain--) {
+		s->percent += s->inc;
+		while (s->percent > 1) s->percent -= 1;
+		if (s->percent <= 0.5) sample += s->vol; }
+	if (sample >= 255.0) return 255;
+	if (sample <= 0.0) return 0;
+	return (byte) sample; }
 
 void freenote (int index) {
 	if (usednotes-1 != index)
@@ -74,33 +83,44 @@ void noteoff (double f) {
 	int index = find(f);
 	if (index != -1) freenote(index); }
 
-void noteon (double f) {
-	if (f <= 0) return;
-	if (usednotes>=NSYNTHS) return;
-	if (-1 != find(f)) return;
-	ns[usednotes++] = (N){0, f/(double)samplerate}; }
+void noteon (double f, double vol) {
+	fprintf(stderr, "noteon %lf %lf\n", f, vol);
+	if (f <= 0) exit(1);
+	double inc = f/(double)samplerate;
+	while (inc > 1.0) inc -= 1.0;
+	int index = find(f);
+	if (index == -1) {
+		if (usednotes < NSYNTHS)
+			ns[usednotes++] = (S){0.0, inc, vol};
+		return; }
+	else
+		ns[index].vol = MAX(vol, ns[index].vol); }
 
+// TODO Derived by hand; might not be perfect.
+// TODO Move this into a midi library?
 double midifreq (byte code) {
 	return powl(2, ((double) code + 36.37361656)/12.0); }
 
 void midirdy () {
-	static byte buf[81];
-	static int bytes;
-	static int i, type, freq;
-	bytes = read(0, buf, 80);
+	byte buf[81];
+	int bytes = read(0, buf, 80);
 	if (-1 == bytes) return;
 	if (!bytes) exit(0);
-	for (i=0; i<bytes; i++) {
-		static byte msg[3];
+	for (int i=0; i<bytes; i++) {
+		byte msg[3];
 		switch (mm_inject(buf[i], msg)) {
 			case 2: exit(1);
 			case 1: continue;
 			case 0: {
-				type = mm_msgtype(msg);
-				freq = midifreq(msg[1]);
-				if (type == MM_NOTEON) noteon(freq);
+				int type = mm_msgtype(msg);
+				double freq = midifreq(msg[1]);
+				// There's nothing special about x/2, it just gives reasonable
+				// results.
+				if (type == MM_NOTEON) noteon(freq, msg[2]/2);
 				if (type == MM_NOTEOFF) noteoff(freq); }}}}
 
+/*
+// This is not used, but it sounds cool.
 void funky () {
 	int i, j=0;
 	for (i=samplerate;; i++) {
@@ -108,20 +128,13 @@ void funky () {
 		if (i > samplerate/(15*NSYNTHS)) {
 			if (usednotes == NSYNTHS) { usednotes = 0; freq *= 1.01; j=0; }
 			j++; noteon(freq * j); i = 0; }}}
-
-void go () { midirdy(); send(nextsample()); }
-
-void noblock (int fd) {
-	int flags = fcntl(fd, F_GETFL);
-	flags = (-1==flags?0:flags) | O_NONBLOCK;
-	fcntl(fd, F_SETFL, flags); }
+*/
 
 int main (int argc, char **argv) {
 	ioctl(1, SNDCTL_DSP_CHANNELS, &mono);
 	ioctl(1, SNDCTL_DSP_SETFMT, &samplesize);
 	ioctl(1, SNDCTL_DSP_SPEED, &samplerate);
 	ioctl(1, SNDCTL_DSP_SETFRAGMENT, &fragment);
-
 	fd_set readfds, writefds;
 	for (;;) {
 		FD_ZERO (&readfds);
