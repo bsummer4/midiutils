0de39e6f51b7259dc2a0c884b4a138aec14af07f
Benjamin Summers <bsummer4@eecs.utk.edu>
2010-07-15 00:22:53 -0400

    Made mticks more accurate by using clock_nanosleep().

--- mticks.c
+++ mticks.c
@@ -8,6 +8,10 @@
 #include "midimsg.h"
 #include <stdio.h>
 #include "util.h"
+#include <stdbool.h>
+#include <limits.h>
+
+#define CLK CLOCK_REALTIME
 
 unsigned long bpm = 120;
 const unsigned long bil = 1000 * 1000 * 1000;
@@ -18,10 +22,21 @@ struct timespec timediff (int bpm) {
 	double spb = 1.0 / tps;
 	return (struct timespec){spb, (long)(spb*bil) % bil}; }
 
+void addto (struct timespec *x, struct timespec *y) {
+	x->tv_sec += y->tv_sec;
+	x->tv_nsec += y->tv_nsec;
+	while (x->tv_nsec > 999999999) x->tv_sec++, x->tv_nsec-=999999999; }
+
 int main (int argc, char **argv) {
+	struct timespec next;
+	E("clock_gettime", clock_gettime(CLK, &next));
 	close(0); // we don't use stdin
 	if (argc>=2) bpm = atol(argv[1]);
-	struct timespec in = timediff(bpm), out;
+	struct timespec diff=timediff(bpm);
 	struct mm_msg m = {MM_CLOCK, 0, 0, 0};
-	do mm_write(1, &m); while (-1 != nanosleep(&in, &out));
+	for (;;) {
+		mm_write(1, &m);
+		addto(&next, &diff);
+		if (clock_nanosleep(CLK, TIMER_ABSTIME, &next, NULL))
+			perr("clock_nanosleep"); }
 	return 0; }
